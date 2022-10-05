143ff678d34523ada4aa0b5dcef6a19f3ff4392f
Obinna <obi.udeogu@gmail.com>
2022-10-04 20:19:50 -0600

    Add new property to getAllTasks endpoint

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,7 +4,7 @@ const { createCustomError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
-  res.status(200).json({ tasks });
+  res.status(200).json({ tasks: tasks, total_Task: tasks.length });
 });
 
 const createTask = asyncWrapper(async (req, res) => {
